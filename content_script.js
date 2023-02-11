4fb3919d6fa5383eff9f865f42e479bb16327249
Zahid Hasan <hzahid2999@gmail.com>
2023-02-11 14:46:56 +0600

    fixed problem

--- content_script.js
+++ content_script.js
@@ -1,42 +1,64 @@
 let items = [];
 let currentPage = 0;
-const showPages = 10;
+let showPages = 10;
 
-const parentDiv = document.createElement("div");
-parentDiv.className = "flex justify-center mt-5";
-parentDiv.innerHTML = `
-  <nav class="flex">
-    <ul class="flex items-center"></ul>
-  </nav>
-`;
+const init = () => {
+  if (window.buttonsInterval) {
+    clearInterval(window.buttonsInterval);
+  }
+  window.buttonsInterval = setInterval(() => {
+    const actionsArea = document.querySelector("form>div>div");
+    if (!actionsArea) {
+      return;
+    }
+    if (shouldAddPagination()) {
+      main();
+    }
+    if (shouldRemovePagination()) {
+      removeButtons();
+    }
+  }, 1000);
+};
 
 const main = () => {
+  const innerItems = [];
+
   document
     .querySelectorAll(
       ".w-full.border-b.border-black\\/10.dark\\:border-gray-900\\/50.text-gray-800.dark\\:text-gray-100.group.dark\\:bg-gray-800"
     )
     .forEach((item, index) => {
       item.classList.add(`question-${index + 1}`);
-      items.push(index + 1);
+      innerItems.push(index + 1);
     });
+
+  items = innerItems;
+
+  createPaginationList();
+  addNextButton();
+  addPrevButton();
+};
+
+const createPaginationList = () => {
+  const parentDiv = document.createElement("div");
+  parentDiv.className = "flex justify-center mt-5 pagination";
+  parentDiv.innerHTML = `
+  <nav class="flex">
+    <ul class="flex items-center"></ul>
+  </nav>
+`;
 
   const childNode = document.querySelector(
     ".px-3.pt-2.pb-3.text-center.text-xs.text-black\\/50.dark\\:text-white\\/50.md\\:px-4.md\\:pt-3.md\\:pb-6"
   );
-  if (childNode !== null) {
-    document
-      ?.querySelector(".absolute.bottom-0")
-      ?.insertBefore(parentDiv, childNode);
 
-    createPaginationList();
-    addNextButton();
-    addPrevButton();
-  }
-};
+  document
+    ?.querySelector(".absolute.bottom-0")
+    ?.insertBefore(parentDiv, childNode);
 
-const createPaginationList = () => {
   const paginationList = document.querySelector("nav ul");
-  const slicedItems = items.slice(currentPage, currentPage + showPages);
+
+  const slicedItems = items.slice(0, showPages);
 
   slicedItems.forEach((item) => {
     const li = document.createElement("li");
@@ -52,9 +74,11 @@ const createPaginationList = () => {
 
     paginationList.appendChild(li);
   });
+  currentPage = 0;
 };
 
 const addNextButton = () => {
+  const parentDiv = document.querySelector(".pagination");
   const nextButton = document.createElement("button");
   nextButton.className =
     "btn flex justify-center gap-2 btn-neutral border-0 md:border next";
@@ -65,6 +89,7 @@ const addNextButton = () => {
 };
 
 const addPrevButton = () => {
+  const parentDiv = document.querySelector(".pagination");
   const prevButton = document.createElement("button");
   prevButton.className =
     "btn flex justify-center gap-2 btn-neutral border-0 md:border prev";
@@ -78,7 +103,6 @@ const handlePagination = (isNext) => {
   isNext ? currentPage++ : currentPage--;
   const startIndex = currentPage;
   const endIndex = showPages + currentPage;
-
   const slicedItems = items.slice(startIndex, endIndex);
   if (slicedItems.length === 0) {
     return;
@@ -106,7 +130,6 @@ const handlePagination = (isNext) => {
 const handleNextButton = () => handlePagination(true);
 
 const handlePrevButton = () => handlePagination(false);
-
 const scrollToView = (event) => {
   event.preventDefault();
   document
@@ -114,12 +137,48 @@ const scrollToView = (event) => {
     .scrollIntoView({ behavior: "smooth" });
 };
 
-var intervalID = setInterval(function () {
-  var getAllQuestions = document.querySelectorAll(
+const shouldRemovePagination = () => {
+  const isThereSpinner = document.querySelector(".animate-spin");
+
+  if (isThereSpinner !== null) {
+    return true;
+  }
+
+  return false;
+};
+
+const shouldAddPagination = () => {
+  const isTherePagination = document.querySelector(".pagination");
+  const isThereChat = document.querySelectorAll(
     ".w-full.border-b.border-black\\/10.dark\\:border-gray-900\\/50.text-gray-800.dark\\:text-gray-100.group.dark\\:bg-gray-800"
   );
-  if (getAllQuestions?.length > 0) {
-    main();
-    clearInterval(intervalID);
+  if (isTherePagination === null && isThereChat?.length > 0) {
+    return true;
   }
-}, 2000);
+  return false;
+};
+
+const removeButtons = () => {
+  const pagination = document.querySelector(".pagination");
+  resetVariable();
+  while (pagination !== null) {
+    pagination.remove();
+    resetVariable();
+  }
+};
+
+const resetVariable = () => {
+  items = [];
+  showPages = 10;
+  currentPage = 0;
+};
+
+// run init
+if (
+  document.readyState === "complete" ||
+  document.readyState === "interactive"
+) {
+  init();
+} else {
+  document.addEventListener("DOMContentLoaded", init);
+}
